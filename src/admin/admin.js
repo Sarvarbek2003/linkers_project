a95bf2a0fda3718aa7e6d8c3b0baa1869c92ba3a
= <=>
2023-02-18 11:56:30 +0500

    [+] IsAdmin Function Deleted!

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -9,35 +9,19 @@ export default async (ctx) => {
       return false;
     }
     await ctx.reply(
-      "Admin Bo'limiga hush kelibsiz!\nKerakli bo'limni tanlang!",
-
+      "Admin Bo'limiga hush kelibsiz!\nKerakli bo'limni tanlang!"
     );
   } catch (e) {
     console.log(e);
   }
 };
 
-async function isAdmin(user_id) {
-  try {
-    const admin = await prisma.users.findMany({
-      where: {
-        user_id: user_id,
-      },
-    });
-    return admin[0].is_admin;
-  } catch (e) {
-    console.log("ERROR: ", e);
-  }
-}
 
 async function showMasters(ctx) {
   try {
-    if (!isAdmin(ctx.from.id)) {
-      ctx.reply("Bu metod siz uchun emas!❌");
-      return false;
-    } else {
-      const masters = await prisma.masters.findMany();
-      ctx.reply("Ustalar ro'yhati:\n", ...Markup.keyboard([]));
-    }
-  } catch (e) {}
+    const masters = await prisma.masters.findMany();
+    ctx.reply("Ustalar ro'yhati:\n", ...Markup.keyboard([]));
+  } catch (e) {
+    console.log(e)
+  }
 }
