9aafcc99dd2057b829e0a8f94c4b29c8db37a848
Sarvarbek <sarvarbekodilov070603@gmail.com>
2023-02-18 14:42:40 +0500

    pagination ready

--- src/bot.js
+++ src/bot.js
@@ -5,11 +5,6 @@ const app = new Telegraf('5924672133:AAFr2B8uMN03TkqqTeiuq-kXYAkSdgCWpXo')
 
 import adminPanel from './admin/admin.js'
 
-app.command('admin', async ctx => {
-    await changeSteep(user, 'admin')
-    await adminPanel(ctx)
-})
-
 app.command('start', async ctx => {
     await changeSteep(user, 'admin')
     await adminPanel(ctx)
@@ -28,14 +23,15 @@ app.hears('Usta', async ctx => {
     const bot = ctx.telegram
     const msg = ctx.message    
     const chat_id = msg.from.id
-    console.log(ctx);
+
     const user = await checkUser(msg)
     await changeSteep(user, 'choose-service')
 
     let keyboard = await selectService()
+    console.log('keyboard31', keyboard);
     bot.sendMessage(chat_id, "Qaysi turdagi xizmatni ko`rsatasiz",{
         reply_markup: {
-            inline_keyboard: keyboard.keyboard
+            inline_keyboard: keyboard
         }
     })
 })
@@ -44,26 +40,6 @@ app.hears('Mijoz', ctx => {
     
 })
 
-app.action('⏩ Keyingisi', async ctx => {
-    const bot = ctx.telegram
-    const msg = ctx.message    
-    const chat_id = msg.from.id
-    const user = await checkUser(msg)
-    await changeSteep(user, 'choose-service')
-
-    let btn = await selectService()
-    console.log(btn);
-    bot.sendMessage(chat_id, "Qaysi turdagi xizmatni ko`rsatasiz",{
-        reply_markup: {
-            resize_keyboard:true,
-            keyboard: btn
-        }
-    })
-})
-
-app.action('⏪ Oldingisi',ctx => {
-    
-})
 
 app.on('text', async (ctx) => {
     const bot = ctx.telegram
@@ -71,25 +47,68 @@ app.on('text', async (ctx) => {
 
     const text = msg.text
     const chat_id = msg.from.id
-    const first_name = msg.from.first_name
     const user = await checkUser(msg)
     const steep = user.steep
     const st = steep[steep.length-1]
 
 })
 
+app.on('callback_query',async ctx => {
+    const bot = ctx.telegram
+    const msg = ctx.update 
+    const chat_id = msg.callback_query.from.id
+    const msgId = msg.callback_query.message.message_id
+    const user = await checkUser(msg.callback_query)
+    console.log('user', msg);
+    const data = msg.callback_query.data
 
+    if(data.split('=')[0]=='prev'){
+        if (data.split('=')[1] == 0) {
+            return bot.answerCbQuery(msg.callback_query.id, "Bu oxirgi saxifa")
+        }
+        let keyboard = await selectService(data.split('=')[1])
+        console.log('keyboard87', keyboard);
+        
+        bot.editMessageText( chat_id, msgId, null,"Qaysi turdagi xizmatni ko`rsatasiz").then(data => {
+            bot.editMessageReplyMarkup(chat_id, msgId, null,{inline_keyboard:keyboard})
+        })
+        
+
+    } else if(data.split('=')[0]=='next'){
+        let keyboard = await selectService(data.split('=')[1])
+        console.log('keyboard97', keyboard);
+
+        await bot.editMessageText( chat_id, msgId, null,"Qaysi turdagi xizmatni ko`rsatasiz" ).then(data => {
+            bot.editMessageReplyMarkup(chat_id, msgId, null,{inline_keyboard:keyboard})
+        })
+
+    }
+})
 
 const selectService = async(page = 1) => {
     try {
-        let services = await prisma.services.findMany({skip: page, take: 5})
+        let services = await prisma.services.findMany()
+        services = services.slice(+page * 10 - 10, 10 * +page)
         let array = []
+        let arr = []
+        let count = 2
         services.forEach(el => { 
-            array.push([{ text:el.service_name, callback_data:el.id }, {text: el.service_name, callback_data:el.id}])           
+            if (count > 0) {
+                arr.push({ text: el.service_name, callback_data:`${el.id}` })           
+                count --
+            } else {
+                array.push(arr)
+                arr = []
+                count = 1
+                arr.push({ text: el.service_name, callback_data:`${el.id}` })     
+            }
+
         });
 
-        array.push([{text: "⏪ Oldingisi", callback_data: 'prev='+page-1}, {text: '⏩ Keyingisi', callback_data:  'next='+page}])
-        return {keyboard: array, page}
+        array.push(arr)
+        array.push([{text: "⏪ Oldingisi", callback_data: 'prev='+(+page-1)}, {text: '⏩ Keyingisi', callback_data: 'next='+(+page+1)}])
+
+        return array
     } catch (error) {
         console.log(error);
         return []
@@ -136,7 +155,7 @@ const changeSteep =async (user, steep, steepHome = false) => {
 }
 
 export {
-    changeSteep
+    changeSteep, app
 }
 app.launch();
 
