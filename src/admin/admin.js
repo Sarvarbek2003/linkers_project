5bb64e99b889f55df337587808b95fc16a1bf214
= <=>
2023-02-18 11:29:49 +0500

    [+] Added is Admin Function

---
+++ src/admin/admin.js
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const isAdmin = async (user_id) => {
+  try {
+    const admin = await prisma.users.findMany({
+      where: {
+        user_id: user_id,
+      },
+    });
+    return admin[0].is_admin;
+  } catch (e) {
+    console.log("ERROR: ", e);
+  }
+};
