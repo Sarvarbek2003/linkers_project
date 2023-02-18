6c057c9dcdda915e42e7bfe4413d3905617a3352
Sarvarbek <sarvarbekodilov070603@gmail.com>
2023-02-18 12:43:16 +0500

    selectServce fundtion added

--- src/bot.js
+++ src/bot.js
@@ -5,6 +5,66 @@ const app = new Telegraf('5924672133:AAFr2B8uMN03TkqqTeiuq-kXYAkSdgCWpXo')
 
 import adminPanel from './admin/admin.js'
 
+app.command('admin', async ctx => {
+    await changeSteep(user, 'admin')
+    await adminPanel(ctx)
+})
+
+app.command('start', async ctx => {
+    await changeSteep(user, 'admin')
+    await adminPanel(ctx)
+
+    bot.sendMessage(chat_id, "Assalomualekum xi MAISHIY XIZMATLAR uchun yartilgan botimizga hush kelibsiz\nKim bo'lib kirmoqchisiz",{
+        reply_markup:{
+            resize_keyboard: true,
+            keyboard:[
+                [{text: "Usta"}, {text:"Mijoz"}]
+            ]
+        }
+    })
+})
+
+app.hears('Usta', async ctx => {
+    const bot = ctx.telegram
+    const msg = ctx.message    
+    const chat_id = msg.from.id
+    console.log(ctx);
+    const user = await checkUser(msg)
+    await changeSteep(user, 'choose-service')
+
+    let keyboard = await selectService()
+    bot.sendMessage(chat_id, "Qaysi turdagi xizmatni ko`rsatasiz",{
+        reply_markup: {
+            inline_keyboard: keyboard.keyboard
+        }
+    })
+})
+
+app.hears('Mijoz', ctx => {
+    
+})
+
+app.action('⏩ Keyingisi', async ctx => {
+    const bot = ctx.telegram
+    const msg = ctx.message    
+    const chat_id = msg.from.id
+    const user = await checkUser(msg)
+    await changeSteep(user, 'choose-service')
+
+    let btn = await selectService()
+    console.log(btn);
+    bot.sendMessage(chat_id, "Qaysi turdagi xizmatni ko`rsatasiz",{
+        reply_markup: {
+            resize_keyboard:true,
+            keyboard: btn
+        }
+    })
+})
+
+app.action('⏪ Oldingisi',ctx => {
+    
+})
+
 app.on('text', async (ctx) => {
     const bot = ctx.telegram
     const msg = ctx.message    
@@ -16,11 +76,25 @@ app.on('text', async (ctx) => {
     const steep = user.steep
     const st = steep[steep.length-1]
 
-    if ((text == '/admin' && user.is_admin) || steep[1] == 'admin') {
-        await changeSteep(user, 'admin')
-        await adminPanel(ctx)
-    } 
 })
+
+
+
+const selectService = async(page = 1) => {
+    try {
+        let services = await prisma.services.findMany({skip: page, take: 5})
+        let array = []
+        services.forEach(el => { 
+            array.push([{ text:el.service_name, callback_data:el.id }, {text: el.service_name, callback_data:el.id}])           
+        });
+
+        array.push([{text: "⏪ Oldingisi", callback_data: 'prev='+page-1}, {text: '⏩ Keyingisi', callback_data:  'next='+page+1}])
+        return {keyboard: array, page}
+    } catch (error) {
+        console.log(error);
+        return []
+    }
+}
 
 
 const checkUser = async(data) => {
@@ -44,11 +118,11 @@ const checkUser = async(data) => {
     }
 }
 
-const changeSteep =async (user, steep) => {
+const changeSteep =async (user, steep, steepHome = false) => {
     try {
         
         let steep = user.steep
-        steep.push(steep)
+        steepHome ? steep = user.steep : steep.push(steep)
         await prisma.users.update({where: {
                 user_id: user.user_id
             },
